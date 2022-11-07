02e8d80d7d9a038ffdaf86775fa378f95eacf29d
bahiyyah_4 <bahiab144@gmail.com>
2022-11-07 15:54:57 +0300

    add lab

---
+++ Clinic_Website/Clinic_app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from django.http import HttpRequest, HttpResponse
+from . import views
+
+app_name = "Clinic_app"
+
+urlpatterns = [
+    path("home/", views.home, name="home"),
+    path("home/add_doctors", views.add_doctor, name="add_doctor"),
+    path("home/updeat_doctors/<doctor_id>",
+         views.update_doctor, name="update_doctor"),
+    path("home/detail/<doctor_id>", views.detail_doctor, name="detail_doctor"),
+    path("home/delete/<doctor_id>", views.delete_doctor, name="delete_doctor"),
+    path("home/doctors/<doctor_id>/appointment",
+         views.add_appointment, name="add_appointment"),
+
+]
