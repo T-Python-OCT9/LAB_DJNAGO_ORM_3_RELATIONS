902b49ffcb623f9263ebd768fd46de7cef4c23bf
abdulelahalyahya <115654046+abdulelahalyahya@users.noreply.github.com>
2022-11-08 16:01:05 +0300

    ORM3

--- website/clinic/models.py
+++ website/clinic/models.py
@@ -2,20 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+class Doctor(model.model):    
+    name = models.CharField(max_length = 256)
+    description = models.TextField()
+    specialization = models.CharField(max_length = 2, choices = DoctorSpec.choices,default = DoctorSpec.general_doc)
+    experience_years = models.IntegerField()
+    rating = models.FloatField()
+
+class Appointment(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete = models.PROTECT)
+    patient_name = models.CharField(max_length = 256)
+    case_description = models.TextField()
+    patient_age = models.IntegerField()
+    appointment_datetime = models.DateField()
+    is_attended = models.BooleanField()
 
     
-    class Doctor(model.model):
-    name= model.CharField(max_length=1024)
-    description = models.TextField()
-    specialization = models.DateTimeField()
-    experience_years = models.ImageField(upload_to="image/")
-rating
-    class appointment(model.model):
-    atient_name = model.CharField(max_length=1024)
-    case_description = models.TextField()
-    patient_age = models.DateTimeField()
-    appointment_datetime = models.ImageField(upload_to="image/")
-is_attended
     
 ### Models
 
